b2924b618f32aea791c7ec39e2743e71f4f088e3
Tim Brandle <tim.brandle@thoughtworks.com>
2021-03-04 22:50:19 -0700

    change return value to object

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -3,8 +3,7 @@ import React, { useState } from 'react'
 const useInput = (defaultValue) => {
   const [value, setValue] = useState(defaultValue)
   const onChange = (e) => setValue(e.target.value)
-  const Input = () => <input value={value} onChange={onChange}/>
-  return [value, Input]
+  return {value, onChange}
 }
 
 export default useInput
